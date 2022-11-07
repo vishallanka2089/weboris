c2da1af898b9ad21f40c60d8ee875db2bc6018db
Syed  Ikram Uddin <111906413+SyedIkram03@users.noreply.github.com>
2022-11-07 15:49:21 +0530

    Add files via upload

---
+++ lab.js
@@ -0,0 +1,33 @@
+let roll 
+let sem
+let sub
+let week
+var subjects = {
+    'PBL Lab': 'ACSC20',
+    'OOSD Lab': 'ACSC21',
+    'WAD Lab': 'AITC10'
+}
+let subb
+
+document.getElementById("btn").onclick = function () {
+
+    
+    roll=document.getElementById("roll").value;
+    
+    sem=document.getElementById("sem").value;
+    sub= document.getElementById("sub").value; 
+    week=document.getElementById("week").value;
+    roll = roll.toUpperCase()
+    subb=subjects[sub]
+    if (roll.includes('95')) {
+    // return `https://iare-data.s3.ap-south-1.amazonaws.com/uploads/STUDENTS/${roll}/LAB/${sem}/${eval(subjects[sub])}/${roll}_week${week}.pdf`;
+
+    let url = `https://iare-data.s3.ap-south-1.amazonaws.com/uploads/STUDENTS/${roll}/LAB/SEM${sem}/${subb}/${roll}_week${week}.pdf`
+    console.log(url);
+    document.getElementById("lin").setAttribute("href", url);
+
+    
+    } else {
+        return alert("Value Incorrect kcpd!");
+    }
+}

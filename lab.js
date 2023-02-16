7bd56824f9266f0a51e77c3cd798a9fa94d88201
vishallanka2089 <104144841+vishallanka2089@users.noreply.github.com>
2023-02-16 11:44:43 +0530

    Updated JS

--- lab.js
+++ lab.js
@@ -1,12 +1,53 @@
+
 let roll 
 let sem
 let sub
 let week
-var subjects = {
-    'PBL Lab': 'ACSC20',
-    'OOSD Lab': 'ACSC21',
-    'WAD Lab': 'AITC10'
+
+const SEM1 = {
+    "PBL Lab": "ACSC20",
+    "OOSD Lab": "ACSC21",
+    "WAD Lab": "AITC10"
+
 }
+
+const SEM2 = {
+    "DMKD LAB":"ACIC08" ,
+    "STL LAB":"ACIC09"
+
+}
+const SEM3 = {
+    "PBL Lab": "ACSC20",
+    "OOSD Lab": "ACSC21",
+    "WAD Lab": "AITC10"
+
+}
+
+const SEM4 = {
+    "DMKD":"ACIC08" ,
+    "STL LAB":"ACIC09"
+
+}
+const SEM5 = {
+    "PBL Lab": "ACSC20",
+    "OOSD Lab": "ACSC21",
+    "WAD Lab": "AITC10"
+
+}
+
+const SEM6 = {
+    "DMKD LAB":"ACIC08" ,
+    "STL LAB":"ACIC09"
+
+}
+
+
+
+
+
+
+
+
 let subb
 
 document.getElementById("btn").onclick = function () {
@@ -18,11 +59,11 @@ document.getElementById("btn").onclick = function () {
     sub= document.getElementById("sub").value; 
     week=document.getElementById("week").value;
     roll = roll.toUpperCase()
-    subb=subjects[sub]
+    subb=eval(sem)[sub]
     if (roll.includes('95')) {
     // return `https://iare-data.s3.ap-south-1.amazonaws.com/uploads/STUDENTS/${roll}/LAB/${sem}/${eval(subjects[sub])}/${roll}_week${week}.pdf`;
 
-    let url = `https://iare-data.s3.ap-south-1.amazonaws.com/uploads/STUDENTS/${roll}/LAB/SEM${sem}/${subb}/${roll}_week${week}.pdf`
+    let url = `https://iare-data.s3.ap-south-1.amazonaws.com/uploads/STUDENTS/${roll}/LAB/${sem}/${subb}/${roll}_week${week}.pdf`
     console.log(url);
     window.open(url);
 
@@ -31,3 +72,21 @@ document.getElementById("btn").onclick = function () {
         return alert("Value Incorrect kcpd!");
     }
 }
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+
+function sub_sem() {
+    semester = document.querySelector('#sem').value;
+    let html = "";
+    for (let i = 0; i < Object.keys(eval(semester)).length; i++) {
+        line = `<option value="${Object.keys(eval(semester))[i]}">${Object.keys(eval(semester))[i]}</option>`;
+        html += line;
+    };
+    //console.log(html);
+    document.getElementById('sub').innerHTML = html;
+
+}
+
+
+
